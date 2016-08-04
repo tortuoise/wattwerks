fce9a145ef64dd119f8850f3a63baf9280315b15
R.C.Sridhar <sanyasinp@gmail.com>
2016-08-04 22:28:40 +0100

    20160804 mercury

--- wattwerks_test.go
+++ wattwerks_test.go
@@ -1,13 +1,38 @@
-package wattwerks
+package wattwerks_test
 
 import (
-	"html/template"
-	"strings"
+	_"fmt"
+	_"encoding/json"
+	"encoding/csv"
+	"bytes"
+	_"io"
+	"io/ioutil"
+	"log"
 	"testing"
+
+	_"appengine"
+	_"appengine/aetest"
+	_"appengine/datastore"
+	_"appengine/user"
 )
 
-func Test(t *Testing.T) {
+func TestCSV(t *testing.T) {
+	raw, err := ioutil.ReadFile("products.csv")
+	handle(err)
+	r := csv.NewReader(bytes.NewReader(raw))
+	//for {
+		records, err := r.ReadAll()
+		if err != nil {
+			log.Println(err)
+		}
+		if len(records) == 0 {
+			t.Errorf("Boo")
+		}
+	//}
+}
 
+func handle(err error) {
 
+	log.Println(err)
 
 }
